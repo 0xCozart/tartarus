ebda806277991258d1146daa5054b3e59ed809e6
alan <alanvega002@gmail.com>
2023-08-21 20:07:36 -0700

    wrapped app with apollo provider and configured a prop to move up the authed provider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,41 @@
+import {
+  ApolloProvider,
+  type ApolloClient,
+  type NormalizedCacheObject,
+} from "@apollo/client";
 import { type AppType } from "next/dist/shared/lib/utils";
+import Head from "next/head";
+import { useEffect, useState } from "react";
+import ComposeApolloClient from "~/api/composedb/client";
 import "~/styles/globals.css";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  const [client, setClient] = useState<ApolloClient<NormalizedCacheObject>>();
+  const [providerSigner, setProviderSigner] = useState();
+
+  useEffect(() => {
+    if (providerSigner) {
+      ComposeApolloClient(providerSigner)
+        .then((authedClient) => setClient(authedClient))
+        .catch(console.error);
+    }
+  }, [providerSigner]);
+
+  if (client)
+    return (
+      <ApolloProvider client={client}>
+        <Head>
+          <title>Nabu</title>
+          <meta name="description" content="DAO social tooling" />
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+        {/* <Layout> */}
+        <Component {...pageProps} setProviderSigner={setProviderSigner} />
+        {/* </Layout> */}
+      </ApolloProvider>
+    );
+
+  return <NoAccessPage />;
 };
 
 export default MyApp;
