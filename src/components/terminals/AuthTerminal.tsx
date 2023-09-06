c9d1802df2235caed445342fcc9f1f532ac71805
alan <alanvega002@gmail.com>
2023-09-05 17:06:26 -0700

    added terminal props

--- src/components/terminals/AuthTerminal.tsx
+++ src/components/terminals/AuthTerminal.tsx
@@ -1,29 +1,51 @@
 "use client";
 
-import { useRef } from "react";
+import { useRef, useState } from "react";
 import Terminal from "react-console-emulator";
 import { type EthProvider } from "~/api/composedb/client";
 
 const AuthTerminal = ({ provider, signer }: EthProvider) => {
   const terminal = useRef<unknown>();
+  const [disable, setDisable] = useState(false);
+
   const commands = {
     login: {
-      description: "enter password:",
+      description: "login command for authorizing Tartarus account",
+      usage: "'login password'",
       fn: (password: string) => {
         if (password.length < 5) return "password is to short to be valid.";
-        return "nice";
+        return `${password}`;
       },
     },
-    signUp: {
-      description: "enter a username...",
-      fn: () => {
-        return "xd";
+    signup: {
+      description: "signup to Tartarus",
+      usage: "'signup username password'",
+      fn: (username: string, password: string) => {
+        return `${username}, ${password}`;
+      },
+      hi: {
+        description: "",
+        usage: "",
+        fn: () => {
+          return "hello, welcome to Tartarus";
+        },
       },
     },
   };
 
   // return <Coquille commands={commands}></Coquille>;
-  return <Terminal ref={terminal} commands={commands} />;
+  return (
+    <Terminal
+      autoFocus={true}
+      disabled={disable}
+      disableOnProcess={true}
+      errorText={"an error has occured, please refresh the page..."}
+      noEchoBack={true}
+      promptLabel={"$>"}
+      ref={terminal}
+      commands={commands}
+    />
+  );
 };
 
 export default AuthTerminal;
