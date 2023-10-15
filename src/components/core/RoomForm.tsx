da32e5596a51de0f41e992c295e248fb4c6e6815
alan <alanvega002@gmail.com>
2023-10-15 12:16:46 -0700

    create room component form styled

--- src/components/core/RoomForm.tsx
+++ src/components/core/RoomForm.tsx
@@ -54,67 +54,69 @@ const RoomForm = ({ profileId }: RoomFormProps) => {
   };
 
   return (
-    <div>
-      <form>
-        <div className="mb-6 grid gap-6 md:grid-cols-2">
-          <div>
-            <label
-              htmlFor="roomName"
-              className="mb-2 block text-sm font-medium text-gray-900 dark:text-white"
-            >
-              Room name
-            </label>
-            <input
-              type="text"
-              id="roomName"
-              className="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500"
-              placeholder="John"
-              required
-              onChange={handleChange}
-            />
-          </div>
-          <div>
-            <label
-              htmlFor="key"
-              className="mb-2 block text-sm font-medium text-gray-900 dark:text-white"
-            >
-              Key
-            </label>
-            <input
-              type="text"
-              id="key"
-              className="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500"
-              placeholder="Doe"
-              required
-              onChange={handleChange}
-            />
-          </div>
-        </div>
-        <div className="mb-6">
-          <label
-            htmlFor="members"
-            className="mb-2 block text-sm font-medium text-gray-900 dark:text-white"
-          >
-            members
-          </label>
-          <input
-            type="text"
-            id="members"
-            className="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500"
-            placeholder="john.doe@company.com"
-            required
-            onChange={handleChange}
-          />
-        </div>
+    <div className="relative flex h-full w-full flex-row content-around">
+      <div className="align-content-start relative flex flex-col p-10">
+        <div className="">Create a chat room...</div>
+        <div className="h-min max-w-sm rounded-lg border border-gray-200 bg-white p-6 shadow dark:border-gray-700 dark:bg-gray-800">
+          <form>
+            <div className="mb-6 grid gap-6 md:grid-cols-2">
+              <div>
+                <label
+                  htmlFor="roomName"
+                  className="mb-2 block text-sm font-medium text-gray-900 dark:text-white"
+                >
+                  Room name
+                </label>
+                <input
+                  type="text"
+                  id="roomName"
+                  className="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500"
+                  required
+                  onChange={handleChange}
+                />
+              </div>
+              <div>
+                <label
+                  htmlFor="key"
+                  className="mb-2 block text-sm font-medium text-gray-900 dark:text-white"
+                >
+                  Key
+                </label>
+                <input
+                  type="text"
+                  id="key"
+                  className="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500"
+                  required
+                  onChange={handleChange}
+                />
+              </div>
+            </div>
+            <div className="mb-6">
+              <label
+                htmlFor="members"
+                className="mb-2 block text-sm font-medium text-gray-900 dark:text-white"
+              >
+                members
+              </label>
+              <input
+                type="text"
+                id="members"
+                className="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500"
+                required
+                onChange={handleChange}
+              />
+            </div>
 
-        <button
-          type="submit"
-          className="w-full rounded-lg bg-blue-700 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 sm:w-auto"
-          onSubmit={void handleSubmit}
-        >
-          Submit
-        </button>
-      </form>
+            <button
+              type="submit"
+              className="w-full rounded-lg bg-blue-700 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 sm:w-auto"
+              onSubmit={void handleSubmit}
+            >
+              Submit
+            </button>
+          </form>
+        </div>
+      </div>
     </div>
   );
 };
