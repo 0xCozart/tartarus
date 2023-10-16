6b8f5f34f2d1059702c4d16b091cfcb4eef6d882
alan <alanvega002@gmail.com>
2023-10-15 19:52:27 -0700

    types for viewer query

--- src/components/core/MainChat.tsx
+++ src/components/core/MainChat.tsx
@@ -6,8 +6,11 @@ import {
   type Dispatch,
   type SetStateAction,
 } from "react";
-import { type RoomsQuery, type TartarusProfile } from "~/__generated__/graphql";
-import { GET_ROOMS } from "~/api/apollo/querys";
+import {
+  type TartarusProfile,
+  type ViewerRoomsWMembersMessagesQuery,
+} from "~/__generated__/graphql";
+import { GET_VIEWER_ROOMS_W_MEMBERS_MESSAGES } from "~/api/apollo/querys";
 
 type MainChatProps = {
   tartarusProfile: TartarusProfile;
@@ -17,13 +20,16 @@ type MainChatProps = {
 function MainChat({ tartarusProfile, setFile }: MainChatProps) {
   const imageUploadRef = useRef<HTMLInputElement>(null);
 
-  const roomsQuery = useQuery(GET_ROOMS);
-  const rooms = roomsQuery.data as RoomsQuery;
-  const { loading, error } = roomsQuery;
+  //
+  const roomsQuery = useQuery(GET_VIEWER_ROOMS_W_MEMBERS_MESSAGES);
+  const { viewer, __typename } =
+    roomsQuery.data as ViewerRoomsWMembersMessagesQuery;
 
   const { displayName } = tartarusProfile;
 
-  console.log({ roomsQuery: roomsQuery.data as RoomsQuery });
+  console.log({ roomsQuery: viewer?.roomList?.edges });
+
+  // this file upload should be turned into a hook or something idk (will be moved)
   const handleFileInputClick = () => {
     if (imageUploadRef.current) imageUploadRef.current.click();
   };
@@ -94,41 +100,43 @@ function MainChat({ tartarusProfile, setFile }: MainChatProps) {
             {/* Room title ends */}
             {/* Room list start */}
             <div className="h-100 -mx-2 mt-4 flex flex-col space-y-1 overflow-y-auto">
-              {
-                <button className="flex flex-row items-center rounded-xl p-2 hover:bg-gray-100">
-                  <div className="flex h-8 w-8 items-center justify-center rounded-full bg-indigo-200">
-                    H
-                  </div>
-                  <div className="ml-2 text-sm font-semibold">Henry Boyd</div>
-                </button>
-              }
-              <button className="flex flex-row items-center rounded-xl p-2 hover:bg-gray-100">
-                <div className="flex h-8 w-8 items-center justify-center rounded-full bg-gray-200">
-                  M
+              {/* eslint-disable-next-line @typescript-eslint/prefer-nullish-coalescing*/}
+              {rooms?.roomIndex?.edges || !loading ? (
+                rooms.roomIndex?.edges?.map((room) => (
+                  <button
+                    className="flex flex-row items-center rounded-xl p-2 hover:bg-gray-100"
+                    key={room?.node?.roomName}
+                  >
+                    {/* can turn this into a room image */}
+                    <div className="flex h-8 w-8 items-center justify-center rounded-full bg-indigo-200">
+                      H
+                    </div>
+                    <div className="ml-2 text-sm font-semibold">
+                      {room?.node?.roomName}
+                    </div>
+                  </button>
+                ))
+              ) : (
+                <div role="status">
+                  <svg
+                    aria-hidden="true"
+                    className="mr-2 h-8 w-8 animate-spin fill-blue-600 text-gray-200 dark:text-gray-600"
+                    viewBox="0 0 100 101"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                      fill="currentColor"
+                    />
+                    <path
+                      d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                      fill="currentFill"
+                    />
+                  </svg>
+                  <span className="sr-only">Loading...</span>
                 </div>
-                <div className="ml-2 text-sm font-semibold">Marta Curtis</div>
-                <div className="ml-auto flex h-4 w-4 items-center justify-center rounded bg-red-500 text-xs leading-none text-white">
-                  2
-                </div>
-              </button>
-              <button className="flex flex-row items-center rounded-xl p-2 hover:bg-gray-100">
-                <div className="flex h-8 w-8 items-center justify-center rounded-full bg-orange-200">
-                  P
-                </div>
-                <div className="ml-2 text-sm font-semibold">Philip Tucker</div>
-              </button>
-              <button className="flex flex-row items-center rounded-xl p-2 hover:bg-gray-100">
-                <div className="flex h-8 w-8 items-center justify-center rounded-full bg-pink-200">
-                  C
-                </div>
-                <div className="ml-2 text-sm font-semibold">Christine Reid</div>
-              </button>
-              <button className="flex flex-row items-center rounded-xl p-2 hover:bg-gray-100">
-                <div className="flex h-8 w-8 items-center justify-center rounded-full bg-purple-200">
-                  J
-                </div>
-                <div className="ml-2 text-sm font-semibold">Jerry Guzman</div>
-              </button>
+              )}
             </div>
             {/* Room list ends */}
             {/* Seperate section starts (will asign us to it later perhaps) */}
